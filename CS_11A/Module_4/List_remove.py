2b261a4738fb7638e0c7ff522faba92af44cd01f
Armando Ortiz <armando.ortiz.jr.19@outlook.com>
2024-02-04 11:06:25 -0600

    Complete remove portion of assignment

--- CS_11A/Module_4/List_remove.py
+++ CS_11A/Module_4/List_remove.py
@@ -10,3 +10,8 @@ print(motorcycles)
 print(popped_motorcycle)
 first_owned = motorcycles.pop(0)
 print("The first motorcycle I owned was a", first_owned)
+
+#remove
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+motorcycles.remove('ducati')
+print(motorcycles)
